22f69fa5f0bee1a063bdd6030f39ff79fecc026e
Elton Aquino <87995437+elton-aq@users.noreply.github.com>
2024-10-25 16:04:37 -0300

    classificacao na tela

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-import src.Dcnn as dcnn
 import src.desenho as dsn
-import cv2
 import os
 
 def main():
 
     # Abre camera e processa o desenho
-    desenho = dsn.inicia_desenho()
-    desenho_reduzido = dsn.ajusta_desenho(desenho)
-    cv2.imwrite('imagem_cinza.png', desenho_reduzido)       
-
-    # Identifica o numero
-    print(f'O numero identificado foi: {dcnn.identificaNumero("imagem_cinza.png")}')
-
+    dsn.inicia_desenho()
+    
     # Remove arquivo temporario
     os.remove('imagem_cinza.png')
 
